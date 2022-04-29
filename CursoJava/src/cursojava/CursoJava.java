ea0287f27901d134947294d7ecddee4f947929ac
ElieltonCarvalho <elieltoncarvalho2@gmail.com>
2022-04-29 16:45:37 -0300

    subindo o projeto java

---
+++ CursoJava/src/cursojava/CursoJava.java
@@ -0,0 +1,16 @@
+package cursojava;
+
+/**
+ *
+ * @author Elielton Carvalho
+ */
+public class CursoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
