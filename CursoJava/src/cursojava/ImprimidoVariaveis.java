4c32e119d61cd21434357b6a286860d3f456f4a8
ElieltonCarvalho <elieltoncarvalho2@gmail.com>
2022-04-30 15:07:11 -0300

    aula 05 - variáveis e tipos primitivos de dados

---
+++ CursoJava/src/cursojava/ImprimidoVariaveis.java
@@ -0,0 +1,21 @@
+package cursojava;
+
+public class ImprimidoVariaveis {
+
+    //Comentário de uma linha
+    /*Comentário de mais de uma linha*/
+
+    /***
+     * Esse é o comentário oficial da documentação java
+     * @param args parâmetro de entrada do método main
+     */
+    /*método principal, ou seja, o ponto de partida para a JVM executar o programa*/
+    
+
+    public static void main(String[] args) {
+        int numero1 = 3, numero2 = 4, soma;
+        soma = numero1 + numero2;
+        System.out.println("a soma é: " + soma);
+    }
+    
+}
