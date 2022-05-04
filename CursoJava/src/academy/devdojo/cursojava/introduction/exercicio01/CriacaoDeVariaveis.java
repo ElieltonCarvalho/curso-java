17fbd8d8e6423281419eee50ecef2a1de34c74dc
ElieltonCarvalho <elieltoncarvalho2@gmail.com>
2022-05-04 14:48:14 -0300

    Commit da exercício de criação de variáveis.

---
+++ CursoJava/src/academy/devdojo/cursojava/introduction/exercicio01/CriacaoDeVariaveis.java
@@ -0,0 +1,12 @@
+package academy.devdojo.cursojava.introduction.exercicio01;
+
+public class CriacaoDeVariaveis {
+    public static void main(String[] args) {
+        String nome = "Elielton Carvalho", endereco = "Rua III do Escuro", dataNascimento = "22/05/1998";
+        double salario = 2000.00;
+
+
+        System.out.println("Eu "+nome+", residente e domiciliado na "+endereco+
+                ", nascido no dia "+dataNascimento+", declaro que recebi o valor de R$"+salario);
+    }
+}
